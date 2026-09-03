a8a562fb861970125a91bba896ae3f3a7e87a055
Marta <martajones@MacBook-Pro-de-Marta.local>
2026-09-03 14:02:49 +0200

    add mannwhitneyu test

--- evaluation/experiment_statistic_4.py
+++ evaluation/experiment_statistic_4.py
@@ -1,22 +1,49 @@
-import pandas as pd
+
 import os
+import pandas as pd
+from scipy.stats import wilcoxon
 
-base_path = os.path.join(
-    os.path.dirname(__file__),
-    "Parameters and results"
-)
+base_path = os.path.join(os.path.dirname(__file__), "Parameters and results")
 
-table_1 = pd.read_csv(os.path.join(base_path, "table_experiment_4_runs_A.csv"))
-table_2 = pd.read_csv(os.path.join(base_path, "table_experiment_4_runs_B.csv"))
+df = pd.read_csv(os.path.join(base_path, "table_experiment_4_runs.csv"))
 
-table_2["repeat"] = table_2["repeat"] + table_1["repeat"].max() + 1
+METRIC = "PE_max"
+#METRIC = "epsilon"
+ALPHA = 0.05
 
-# Unificar los dos CSV
-result = pd.concat([table_1, table_2], ignore_index=True)
+results = []
 
-# Ordenar
-result = result.sort_values(by=["distribution", "dataset_size", "method", "repeat"]).reset_index(drop=True)
+for dist, group in df.groupby("distribution"):
+    methods = group["method"].unique()
 
-output_path = os.path.join(base_path, "table_experiment_4_runs_AB.csv")
+    m1, m2 = methods
 
-result.to_csv(output_path, index=False)
+    pivot = group.pivot_table(
+        index=["dataset_size", "repeat"], columns="method", values=METRIC
+    )
+    pivot = pivot.dropna(subset=[m1, m2])  # nos quedamos solo con pares completos
+ 
+    x = pivot[m1]
+    y = pivot[m2]
+ 
+    stat, p_value = wilcoxon(x, y, alternative="two-sided", method="approx", zero_method="wilcox")
+    z_stat = wilcoxon(x, y, alternative="two-sided", method="approx", zero_method="wilcox").zstatistic
+ 
+    n_pares = len(pivot)
+    r_effect_size = abs(z_stat) / (n_pares ** 0.5)
+
+    results.append({
+        "distribution": dist,
+        "method_1": m1,
+        "method_2": m2,
+        "W_statistic": stat,
+        "Z_statistic": z_stat,
+        "p_value": p_value,
+        "r_effect_size": r_effect_size,
+        "reject_H0 (p<0.05)": p_value < ALPHA,
+    })
+
+results_df = pd.DataFrame(results).sort_values("distribution").reset_index(drop=True)
+
+output_path = os.path.join(base_path, "table_mannwhitney.csv")
+results_df.to_csv(output_path, index=False)
